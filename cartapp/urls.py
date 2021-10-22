1d49d9ae54740e86409271ba640b5a6949bd2745
olowu abayomi <aayobam@gmail.com>
2021-10-22 16:37:11 +0100

    fixed delete and update button

--- cartapp/urls.py
+++ cartapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import cart_summary, add_to_cart
+from .views import *
 
 
 urlpatterns = [
     path('cart/', cart_summary, name="cart_summary"),
     path('add/', add_to_cart, name="add_to_cart"),
+    path('delete-from-cart/', delete_from_cart, name='delete_from_cart'),
+    path('update-cart/', update_cart, name='update_cart'),
 ]
--- cartapp/views.py
+++ cartapp/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import JsonResponse
 from ecommerceapp.models import Product
 from .cart import Cart
-from django.contrib import messages
 
 
+
+
+# holds all product(s) data
 def cart_summary(request):
     template_name = 'cart/cart_summary.html'
     cart = Cart(request)
@@ -12,6 +14,7 @@ def cart_summary(request):
     return render(request, template_name, context)
 
 
+# adds product(s) to cart
 def add_to_cart(request):
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
@@ -21,5 +24,30 @@ def add_to_cart(request):
         cart.add(product=product, product_qty=product_qty)
         cart_qty = cart.__len__()
         response = JsonResponse({'qty':cart_qty})
-        print(f"product id is {product_id}, product quantity is {product_qty} and product price is {product.price}")
         return response
+
+
+# deletes product(s) from cart
+def delete_from_cart(request):
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get("productid"))
+        cart = Cart(request)
+        cart.delete(product=product_id)
+        cart_qty = cart.__len__()
+        cart_total = cart.get_total_price()
+        response = JsonResponse({"qty":cart_qty, 'subtotal':cart_total})
+        return response
+
+
+#update product(s) data in cart
+def update_cart(request):
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty')) 
+        cart = Cart(request)
+        cart.update(product=product_id, qty=product_qty)
+        cart_qty = cart.__len__()
+        cart_total = cart.get_total_price()
+        response = JsonResponse({"qty":cart_qty, "subtotal":cart_total})
+        return response
+       
