3000fe6c8878c2abceee728910ac2cab5122c9b8
Olowu Abayomi <aayobam@gmail.com>
2021-10-30 12:45:45 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -70,7 +70,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class Profile(models.Model):
-    #user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile)
     address = models.CharField(max_length=250)
     zipcode = models.CharField(max_length=10)
     city = models.CharField(max_length=100)
