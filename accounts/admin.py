dd0a3294ef4e0da18590d4c5d2ace7ae54242fc8
Olowu Abayomi <aayobam@gmail.com>
2021-11-01 04:24:03 +0100

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from .models import Profile
 
 
 
-@admin.register(Profile)
-class AdminProfile(admin.ModelAdmin):
-    list_display = ('user', 'address', 'postal_code', 'city', 'state', 'country', 'phone_no', 'profile_image', 'created_at', 'updated_at')
-    list_filter = ('phone_no',)
+# @admin.register(Profile)
+# class AdminProfile(admin.ModelAdmin):
+#     list_display = ('user', 'address', 'postal_code', 'city', 'state', 'country', 'phone_no', 'profile_image', 'created_at', 'updated_at')
+#     list_filter = ('phone_no',)
