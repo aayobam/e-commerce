346758bf75c29cf85534d8973d13553a21f695fa
Olowu Abayomi <aayobam@gmail.com>
2021-11-01 04:19:29 +0100

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Profile
 
 
-admin.site.register(Profile)
 
-# @admin.register(Profile)
-# class AdminProfile(admin.ModelAdmin):
-#     list_display = ('user', 'address', 'zipcode', 'city', 'state', 'country', 'phone_no', 'profile_image', 'created_at', 'updated_at')
-#     list_filter = ('phone_no',)
+
+@admin.register(Profile)
+class AdminProfile(admin.ModelAdmin):
+    list_display = ('user', 'address', 'postal_code', 'city', 'state', 'country', 'phone_no', 'profile_image', 'created_at', 'updated_at')
+    list_filter = ('phone_no',)
