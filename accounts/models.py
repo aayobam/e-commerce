015341e445d12d3eb331ec67f1177113abdf16c7
Olowu Abayomi <aayobam@gmail.com>
2021-11-01 04:15:01 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -71,10 +71,10 @@ from django_countries.fields import CountryField
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.CharField(max_length=250)
-    #zipcode = models.CharField(max_length=10)
+    zipcode = models.CharField(max_length=10)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
-    #country = CountryField()
+    country = CountryField()
     phone_no = PhoneField(help_text="your phone number")
     profile_image = models.ImageField(upload_to="media/images", default="default.jpg")
     created_at = models.DateTimeField(auto_now_add=True)
