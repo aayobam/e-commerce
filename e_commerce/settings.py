7037458a8e0262d9aaabad9bf2f6c866dc3a7ce8
Olowu Abayomi <aayobam@gmail.com>
2022-01-24 12:51:01 +0100

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -20,9 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-if DEBUG == False:
-    ALLOWED_HOSTS = ['http://ghostretail.herokuapp.com', '127.0.0.1', 'localhost']
+DEBUG = False
+ALLOWED_HOSTS = ['https://ghostretail.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 
