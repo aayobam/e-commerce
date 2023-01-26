dfcbf1a4ea69f099d126c0b2e55ac6aba3d65a79
Abayomi Olowu <aayobam@gmail.com>
2023-01-26 16:33:07 +0100

    Update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['https://ghostretail.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 
